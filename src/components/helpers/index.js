b0cca77ad6f2f294d0521b4016531b12c905e04b
Moises Fuentes <dev.gze.moises@gmail.com>
2022-08-27 10:15:45 -0500

    Funcion para definir titulos

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -7,3 +7,32 @@ export const passwordStrong = password => {
     const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
     return re.test(password)
 }
+
+
+export const defineTitle = (value) => {
+    let title = value
+
+    switch (value) {
+
+        case 'followers':
+            title = 'Total de seguidores'
+            break;
+
+        case 'likes':
+            title = 'Total de me gustas'
+            break;
+
+        case 'views':
+            title = 'Vistas a mi perfil'
+            break;
+
+        case 'news':
+            title = 'Nuevos seguidores'
+            break;
+
+        default:
+            title = ''
+            break;
+    }
+    return title
+}
