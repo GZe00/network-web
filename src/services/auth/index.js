d3ce8a5eef4ddf5e9be887a579c4297c2850882c
Moises Fuentes <dev.gze.moises@gmail.com>
2022-08-30 06:00:38 -0500

    Proceso de verificacion de token, se tiene que continuar

---
+++ src/services/auth/index.js
@@ -0,0 +1,16 @@
+import { api } from "../api"
+
+export const validateTokenLocalStorage = async (token) => {
+    return new Promise((resolve, reject) => {
+        try {
+            api.post("/auth/network/api/validate_token", token).then(response => {
+                console.log("Dentro de la funcion validateTokenLocalStorage")
+                console.log(response)
+                resolve(true)
+            })
+        } catch (error) {
+            console.log(error);
+            reject(false)
+        }
+    })
+}
